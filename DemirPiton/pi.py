4eaa2faace045340c9ed04350ffeea57ab853b5b
dmrarikan <dmrarikan@gmail.com>
2021-02-05 01:18:21 +0100

    pi approximation using python done

--- DemirPiton/pi.py
+++ DemirPiton/pi.py
@@ -1,5 +1,14 @@
 import random
-for x in range(1000):
+import math
+
+a = 0
+b = 100000
+for i in range(b):
     x = random.random()
     y = random.random()
-    #ATAMERT KODUMU OKUMA LAN 
+    dist = math.sqrt(((x**2)+(y**2)))
+    if dist <= 1 : a += 1
+
+
+pi = ((a/b) * 4)
+print(pi)
